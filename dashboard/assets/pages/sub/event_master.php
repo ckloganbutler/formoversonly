de11a04366793c6148cf389004992a2c8daf5715
Logan Butler <ckloganbutler@yahoo.com>
2017-07-03 05:58:04 -0400

    updates

--- dashboard/assets/pages/sub/event_master.php
+++ dashboard/assets/pages/sub/event_master.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['logged'])){
                 <form action="#" class="form-horizontal" id="submit_form" method="POST">
                     <div class="form-wizard">
                         <div class="form-body">
-                            <ul class="nav nav-pills nav-justified steps">
+                            <ul class="nav nav-pills nav-justified steps hidden">
                                 <li>
                                     <a href="#tab1" data-toggle="tab" class="step">
                                         <span class="number">1 </span>
@@ -41,15 +41,57 @@ if(isset($_SESSION['logged'])){
                                 </div>
                             </div>
                             <div class="tab-content">
-                                <div class="alert alert-danger display-none">
-                                    <button class="close" data-dismiss="alert"></button>
-                                    You have some form errors. Please check below.
-                                </div>
                                 <div class="tab-pane" id="tab1">
-
+                                    <h3>Payment Details <small class="hidden-xs hidden-sm"><span class="text-danger">| </span>verify invoice matches amount due.</small> <span class="pull-right">Amount due: <strong><span class="text-success">$X,XXX</span></strong></span></h3>
                                 </div>
                                 <div class="tab-pane" id="tab3">
-
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="form-group">
+                                                <label class="control-label col-md-3">Tender Type <span class="required">*</span></label>
+                                                <div class="col-md-9">
+                                                    <select class="form-control input-sm type" name="type" data-target=".tender-inputs">
+                                                        <option disabled selected value="">Select one..</option>
+                                                        <option value="Cash" data-show=".cash">Cash</option>
+                                                        <option value="Check" data-show=".chec">Check</option>
+                                                        <option value="Credit/Debt" data-show=".cc">Credit/Debt Card (ckPay&trade;)</option>
+                                                        <option value="Other" data-show=".other">Credit/Debt Card (Other Payment Processor)</option>
+                                                    </select>
+                                                    <span class="help-block">This should the type of payment in which the customer choose to use.</span>
+                                                </div>
+                                            </div>
+                                            <div class="tender-inputs">
+                                                <div class="form-group cash hidden">
+                                                    <label class="control-label col-md-3">Tender Amount <span class="required">*</span></label>
+                                                    <div class="col-md-9">
+                                                        <input type="number" step="any" class="form-control input-sm" name="amount" placeholder="XXXX.XX">
+                                                        <span class="help-block">This is the amount you are receiving in cash.</span>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group chec hidden">
+                                                    <label class="control-label col-md-3">Tender Amount <span class="required">*</span></label>
+                                                    <div class="col-md-9">
+                                                        <input type="number" step="any" class="form-control input-sm" name="amount" placeholder="XXXX.XX">
+                                                        <span class="help-block">This is the amount you are receiving from this check.</span>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group cc hidden">
+                                                    <label class="control-label col-md-3">Tender Amount <span class="required">*</span></label>
+                                                    <div class="col-md-9">
+                                                        <input type="number" step="any" class="form-control input-sm" name="amount" placeholder="XXXX.XX">
+                                                        <span class="help-block">This is the amount you are charging the card for.</span>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group other hidden">
+                                                    <label class="control-label col-md-3">Tender Amount <span class="required">*</span></label>
+                                                    <div class="col-md-9">
+                                                        <input type="number" step="any" class="form-control input-sm" name="amount" placeholder="XXXX.XX">
+                                                        <span class="help-block">This is the amount you are receiving from another type of payment.</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="tab-pane" id="tab4">
 
@@ -61,8 +103,8 @@ if(isset($_SESSION['logged'])){
                                 <div class="col-md-12">
                                     <button href="javascript:;" class="btn default button-previous pull-left">
                                         <i class="m-icon-swapleft"></i> Back </button>
-                                    <button href="javascript:;" class="btn yellow button-save pull-left" type="submit" name="status" value="0">
-                                        Save for later <i class="fa fa-download"></i>
+                                    <button href="javascript:;" class="btn red button-cancel pull-left" type="submit" name="status" value="0">
+                                        Cancel <i class="fa fa-times"></i>
                                     </button>
                                     <button href="javascript:;" class="btn blue button-next pull-right">
                                         Continue <i class="m-icon-swapright m-icon-white"></i>
@@ -80,6 +122,7 @@ if(isset($_SESSION['logged'])){
     </div>
     <script>
         $(document).ready(function() {
+
             var form = $('#submit_form');
             var error = $('.alert-danger', form);
             var success = $('.alert-success', form);
@@ -90,14 +133,18 @@ if(isset($_SESSION['logged'])){
                 errorClass: 'help-block help-block-error', // default input error message class
                 focusInvalid: false, // do not focus the last invalid input
                 rules: {
-
+                    type: {
+                        required: true
+                    },
+                    amount: {
+                        required: true
+                    }
                 },
 
 
                 invalidHandler: function (event, validator) { //display error alert on form submit
                     success.hide();
                     error.show();
-                    Metronic.scrollTo(error, -200);
                 },
 
                 highlight: function (element) { // hightlight error inputs
@@ -130,28 +177,6 @@ if(isset($_SESSION['logged'])){
 
             });
 
-            var displayConfirm = function() {
-                $('#tab4 .form-control-static', form).each(function(){
-                    var input = $('[name="'+$(this).attr("data-display")+'"]', form);
-                    if (input.is(":radio")) {
-                        input = $('[name="'+$(this).attr("data-display")+'"]:checked', form);
-                    }
-                    if (input.is(":text") || input.is("textarea")) {
-                        $(this).html(input.val());
-                    } else if (input.is("select")) {
-                        $(this).html(input.find('option:selected').text());
-                    } else if (input.is(":radio") && input.is(":checked")) {
-                        $(this).html(input.attr("data-title"));
-                    } else if ($(this).attr("data-display") == 'payment[]') {
-                        var payment = [];
-                        $('[name="payment[]"]:checked', form).each(function(){
-                            payment.push($(this).attr('data-title'));
-                        });
-                        $(this).html(payment.join("<br>"));
-                    }
-                });
-            }
-
             var handleTitle = function(tab, navigation, index) {
                 var total = navigation.find('li').length;
                 var current = index + 1;
@@ -173,13 +198,11 @@ if(isset($_SESSION['logged'])){
                 if (current >= total) {
                     $('#payments').find('.button-next').hide();
                     $('#payments').find('.button-submit').show();
-                    displayConfirm();
                 } else {
                     $('#payments').find('.button-next').show();
                     $('#payments').find('.button-submit').hide();
                 }
-                Metronic.scrollTo($('.page-title'));
-            }
+            };
 
             // default form wizard
             $('#payments').bootstrapWizard({
@@ -225,9 +248,26 @@ if(isset($_SESSION['logged'])){
             $('#payments').find('.button-previous').hide();
             $('#payments .button-submit').click(function () {
                 Pace.track(function(){
-                   
+
                 });
             }).hide();
+
+            $('.type').on('change', function() {
+                var type    = $(this).val();
+                var target  = $(this).data('target');
+                var show   =  $("option:selected", this).data('show');
+                $(target).children().addClass('hidden');
+                $(show).removeClass('hidden');
+                $("input:hidden").attr('disabled', 'disabled');
+                $("input:visible").removeAttr('disabled');
+                $('input[name="amount"]').focus();
+                /*
+                if(type == 'Credit/Debt'){
+                    $('#cc_btn').addClass("hide");
+                } else {
+                    $('#cc_btn').removeClass("hide");
+                }*/
+            });
         });
     </script>
     <?php
