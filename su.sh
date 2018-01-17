912cc695562ecd3e80b5a08b8ccd1f125e17a5f0
Logan Butler <ckloganbutler@yahoo.com>
2018-01-17 02:54:32 -0500

    More cleanup

--- su.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#----------------------------------------------------------
-# Simple script to run a PHP script to check storage contracts
-#----------------------------------------------------------
-
-# (1) run the php script!
-php /var/www/dashboard/assets/app/cron.php
